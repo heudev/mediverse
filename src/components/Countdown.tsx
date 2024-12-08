f849f96a83ae8e2f39dbc24c2bf0fe91b7d04aa3
heudev <hulkienesuysal@gmail.com>
2024-12-08 22:41:45 +0300

    add hero

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function CountdownTimer() {
+    const [timeLeft, setTimeLeft] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    });
+
+    useEffect(() => {
+        const targetDate = new Date("2025-02-05T09:00:00");
+
+        const calculateTimeLeft = () => {
+            const currentDate = new Date();
+            const difference = targetDate.getTime() - currentDate.getTime();
+
+            if (difference > 0) {
+                const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+                const minutes = Math.floor((difference / 1000 / 60) % 60);
+                const seconds = Math.floor((difference / 1000) % 60);
+
+                setTimeLeft({ days, hours, minutes, seconds });
+            } else {
+                setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+            }
+        };
+
+        calculateTimeLeft();
+
+        const interval = setInterval(calculateTimeLeft, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+            <div className="flex flex-col p-2 bg-base-200 rounded-box text-black border border-black">
+                <span className="countdown font-mono text-4xl md:text-4xl">
+                    {
+                        timeLeft.days <= 99 ? (
+                            <span style={{ "--value": timeLeft.days.toString().padStart(2, '0') } as React.CSSProperties}></span>
+                        ) : (
+                            String(timeLeft.days).padStart(2, '0')
+                        )
+                    }
+                </span>
+                gün
+            </div>
+            <div className="flex flex-col p-2 bg-base-200 rounded-box text-black  border border-black">
+                <span className="countdown font-mono text-4xl md:text-4xl">
+                    <span style={{ "--value": timeLeft.hours } as React.CSSProperties}></span>
+                </span>
+                saat
+            </div>
+            <div className="flex flex-col p-2 bg-base-200 rounded-box text-black  border border-black">
+                <span className="countdown font-mono text-4xl md:text-4xl">
+                    <span style={{ "--value": timeLeft.minutes } as React.CSSProperties}></span>
+                </span>
+                dk
+            </div>
+            <div className="flex flex-col p-2 bg-base-200 rounded-box text-black  border border-black">
+                <span className="countdown font-mono text-4xl md:text-4xl">
+                    <span style={{ "--value": timeLeft.seconds } as React.CSSProperties}></span>
+                </span>
+                sn
+            </div>
+        </div>
+    );
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+export default function Navbar() {
+    return (
+        <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                {/* Navbar */}
+                <div className="navbar bg-base-300 w-full">
+                    <div className="mx-2 flex-1 px-2 font-bold text-primary">MEDIVERSE</div>
+                    <div className="hidden flex-none xl:block">
+                        <ul className="menu menu-horizontal text-xs text-primary font-bold">
+                            {/* Navbar menu content here */}
+                            <li><a className="hover:text-secondary">MEDIVERSE NEDİR?</a></li>
+                            <li><a className="hover:text-secondary">ORGANİZASYON KOMİTESİ</a></li>
+                            <li><a className="hover:text-secondary">PROGRAM AKIŞI</a></li>
+                            <li><a className="hover:text-secondary">KONUŞMACILAR HAKKINDA</a></li>
+                            <li><a className="hover:text-secondary">ATÖLYELERİMİZ</a></li>
+                            <li><a className="hover:text-secondary">POSTER SUNUMLARI</a></li>
+                            <li><a className="hover:text-secondary">SPONSORLARIMIZ</a></li>
+                        </ul>
+                    </div>
+                    <div className="flex-none xl:hidden">
+                        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="inline-block h-6 w-6 stroke-current text-primary">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div className="drawer-side z-50">
+                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu bg-base-200 min-h-full w-80 p-4 text-primary font-bold">
+                    {/* Sidebar content here */}
+                    <li><a className="hover:text-secondary">MEDIVERSE NEDİR?</a></li>
+                    <li><a className="hover:text-secondary">ORGANİZASYON KOMİTESİ</a></li>
+                    <li><a className="hover:text-secondary">PROGRAM AKIŞI</a></li>
+                    <li><a className="hover:text-secondary">KONUŞMACILAR HAKKINDA</a></li>
+                    <li><a className="hover:text-secondary">ATÖLYELERİMİZ</a></li>
+                    <li><a className="hover:text-secondary">POSTER SUNUMLARI</a></li>
+                    <li><a className="hover:text-secondary">SPONSORLARIMIZ</a></li>
+                </ul>
+                <div className="p-4 font-bold mt-auto text-primary">MEDIVERSE</div>
+            </div>
+        </div>
+    )
+}
